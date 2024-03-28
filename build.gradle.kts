e3320de5c13d3d1c31fd0d30b6f21977b5501a4c
Alexander Mikhalchenko <alexandr941989@gmail.com>
2024-03-28 13:05:32 +0200

    Add login feature.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.1.2"
-	id("io.spring.dependency-management") version "1.1.2"
-	id("com.vaadin") version "24.1.4"
-	kotlin("jvm") version "1.8.22"
-	kotlin("plugin.spring") version "1.8.22"
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("com.vaadin") version "24.3.8"
+	kotlin("jvm") version "2.0.0-Beta5"
+	kotlin("plugin.spring") version "2.0.0-Beta5"
 }
 
 group = "com.anahoret"
@@ -15,20 +15,30 @@ java {
 	sourceCompatibility = JavaVersion.VERSION_17
 }
 
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
+
 repositories {
 	mavenCentral()
 }
 
-extra["vaadinVersion"] = "24.1.4"
 
 dependencies {
 	implementation("com.vaadin:vaadin-spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.boot:spring-boot-starter-security")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.seleniumhq.selenium:selenium-java")
-	testImplementation("io.github.bonigarcia:webdrivermanager:5.4.1")
+	testImplementation("io.github.bonigarcia:webdrivermanager:5.7.0")
 }
+
+extra["vaadinVersion"] = "24.3.8"
 
 dependencyManagement {
 	imports {
--- src/main/kotlin/com/anahoret/vaadinplayground/VaadinPlaygroundApplication.kt
+++ src/main/kotlin/com/anahoret/vaadinplayground/VaadinPlaygroundApplication.kt
@@ -1,44 +1,145 @@
 package com.anahoret.vaadinplayground
 
-import com.vaadin.flow.component.Key
+import com.vaadin.flow.component.ClickEvent
+import com.vaadin.flow.component.applayout.AppLayout
 import com.vaadin.flow.component.button.Button
-import com.vaadin.flow.component.button.ButtonVariant
-import com.vaadin.flow.component.notification.Notification
+import com.vaadin.flow.component.html.H1
+import com.vaadin.flow.component.html.Span
+import com.vaadin.flow.component.login.LoginForm
 import com.vaadin.flow.component.orderedlayout.FlexComponent
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout
-import com.vaadin.flow.component.page.AppShellConfigurator
-import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import com.vaadin.flow.router.BeforeEnterEvent
+import com.vaadin.flow.router.BeforeEnterObserver
 import com.vaadin.flow.router.PageTitle
 import com.vaadin.flow.router.Route
-import com.vaadin.flow.theme.Theme
+import com.vaadin.flow.server.auth.AnonymousAllowed
+import com.vaadin.flow.spring.security.AuthenticationContext
+import com.vaadin.flow.spring.security.VaadinWebSecurity
+import jakarta.annotation.security.PermitAll
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer.AuthorizationManagerRequestMatcherRegistry
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @SpringBootApplication
-@Theme(value = "playground")
-class VaadinPlaygroundApplication : AppShellConfigurator
+class VaadinPlaygroundApplication
 
 fun main(args: Array<String>) {
-	runApplication<VaadinPlaygroundApplication>(*args)
+    runApplication<VaadinPlaygroundApplication>(*args)
 }
 
+@Configuration
+@EnableWebSecurity
+class SecurityConfig: VaadinWebSecurity() {
+
+    override fun configure(http: HttpSecurity) {
+        http.authorizeHttpRequests { auth ->
+            auth.requestMatchers(
+                AntPathRequestMatcher.antMatcher(HttpMethod.GET, "/public/**")
+            ).permitAll()
+        }
+        super.configure(http)
+        setLoginView(http, MainView::class.java)
+    }
+
+    @Bean
+    fun users(): UserDetailsService {
+        val user: UserDetails = User.builder()
+            .username("user") // password = password with this hash, don't tell anybody :-)
+            .password("{bcrypt}$2a$10\$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+            .roles("USER")
+            .build()
+        val admin: UserDetails = User.builder()
+            .username("admin")
+            .password("{bcrypt}$2a$10\$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+            .roles("USER", "ADMIN")
+            .build()
+        return InMemoryUserDetailsManager(user, admin)
+    }
+
+}
+
+class MainLayout(private val authContext: AuthenticationContext) : AppLayout() {
+    init {
+        val logo = H1("Vaadin CRM")
+        logo.addClassName("logo")
+        val header: HorizontalLayout =
+            authContext.getAuthenticatedUser(UserDetails::class.java)
+                .map { user ->
+                    val logout = Button("Logout") { click: ClickEvent<Button?>? -> this.authContext.logout() }
+                    val loggedUser = Span("Welcome " + user.getUsername())
+                    HorizontalLayout(logo, loggedUser, logout)
+                }.orElseGet { HorizontalLayout(logo) }
+
+
+        // Other page components omitted.
+        addToNavbar(header)
+    }
+}
+
+
 @PageTitle("Main")
-@Route("/")
-class MainView() : HorizontalLayout() {
+@Route("", layout = MainLayout::class)
+@AnonymousAllowed
+class MainView : VerticalLayout(), BeforeEnterObserver {
 
-	private val name: TextField = TextField("Your name")
-	private val sayHello: Button = Button("Say hello")
+    private val login = LoginForm()
 
-	init {
-		name.setId("name-input")
-		sayHello.setId("submit-button")
+    init {
+        addClassName("login-view")
+        setSizeFull()
 
-		sayHello.addClickListener { e -> Notification.show("Hello " + name.value) }
-		sayHello.addClickShortcut(Key.ENTER)
-		isMargin = true
-		setVerticalComponentAlignment(FlexComponent.Alignment.END, name, sayHello)
-		sayHello.addThemeVariants(ButtonVariant.LUMO_PRIMARY)
-		add(name, sayHello)
-	}
+        alignItems = FlexComponent.Alignment.CENTER
+        justifyContentMode = JustifyContentMode.CENTER
 
+        login.action = "login"
+
+        add(H1("Vaadin CRM"), login)
+    }
+
+    override fun beforeEnter(beforeEnterEvent: BeforeEnterEvent) {
+        // inform the user about an authentication error
+        if (beforeEnterEvent.location
+                .queryParameters
+                .parameters
+                .containsKey("error")
+        ) {
+            login.isError = true
+        }
+    }
+
+}
+
+@PageTitle("Private page")
+@Route("private", layout = MainLayout::class)
+@PermitAll
+class PrivateView() : HorizontalLayout() {
+    private val label: Span = Span("This is private page")
+
+    init {
+        add(label)
+    }
+}
+
+@PageTitle("Public page")
+@Route("public", layout = MainLayout::class)
+@AnonymousAllowed
+class PublicView() : HorizontalLayout() {
+    private val label: Span = Span("This is public page")
+
+    init {
+        add(label)
+    }
 }
